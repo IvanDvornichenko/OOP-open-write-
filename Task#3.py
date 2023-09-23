94bd09da50334ba26cc500a32aa610998aa2cb22
Ivan Dvornichenko <acsahir1990@mail.ru>
2023-09-24 00:37:39 +0300

    Reworked solution to task 3

--- Task#3.py
+++ Task#3.py
@@ -1,53 +1,29 @@
-with open("1.txt", encoding='utf-8') as f:
-    list_1 = []
-    for idx, line in enumerate(f):
-        list_1.append({idx + 1:line[:-1]})
-    countris_1 = {"1.txt": list_1}
+# coding = utf-8
+text = {}
 
 
-with open("2.txt", encoding='utf-8') as f:
-    list_2 = []
-    for idx, line in enumerate(f):
-        list_2.append({idx + 1: line[:-1]})
-    countris_2 = {"2.txt": list_2}
-
-with open("3.txt", encoding='utf-8') as f:
-    list_3 = []
-    for idx, line in enumerate(f):
-        list_3.append({idx + 1: line[:-1]})
-    countris_3 = {"3.txt": list_3}
+def open_file(names):
+    """Функция открывает файл и создание словарь"""
+    for name in names:
+        with open(name, encoding='utf-8') as f:
+            line = []
+            for l in f:
+                line.append(l)
+            text[len(line)] = {name: line}
 
 
-with open("sorted_file.txt", 'w', encoding='cp1251') as f:
-    count = 0
-    counter = 1
-    while count < 3:
-        if len(list_1) == counter:
-            for key, value in countris_1.items():
-                f.write(f"{key}\n{len(list_1)}\n")
-                for i in value:
-                    for k, v in i.items():
-                        f.write(v + "\n")
+def writ_file(text):
+    """Функция сортирует словарь и записывает в файл sorted_file"""
+    with open("sorted_file.txt", 'w', encoding='cp1251') as f:
+        sorted_text = (sorted(text.items()))
+        for i in sorted_text:
+            for key, val in i[1].items():
+                f.write(f"{key}\n{i[0]}\n")
+                for a in val:
+                    f.write(f"{a}")
                 f.write(f"\n")
-            count += 1
-        if len(list_2) == counter:
-            for key, value in countris_2.items():
-                f.write(f"{key}\n{len(list_2)}\n")
-                for i in value:
-                    for k, v in i.items():
-                        f.write(v + "\n")
-                f.write(f"\n")
-            count += 1
-        if len(list_3) == counter:
-            for key, value in countris_3.items():
-                f.write(f"{key}\n{len(list_3)}\n")
-                for i in value:
-                    for k, v in i.items():
-                        f.write(v + "\n")
-                f.write(f"\n")
-            count += 1
-        counter += 1
+            f.write(f"\n")
 
-with open("sorted_file.txt") as f:
-    for l in f:
-        print(l.strip())
+
+open_file(["1.txt", "2.txt", "3.txt"])
+writ_file(text)
